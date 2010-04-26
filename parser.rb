4424db51166c10a251496fc5f5e134e2c58b924d
unknown <Kristoffer@.(none)>
2010-04-26 20:50:34 +0200

    HR data inside :)

--- parser.rb
+++ parser.rb
@@ -1,40 +1,46 @@
+require 'date'
 require 'fileutils'
 
 class Exersice
+  attr_accessor :max_hr, :length, :resting_hr, :vo2_max, :weight, :hr_data
+
   def date(day, time)
     @date = DateTime.parse "#{day[0..3]}-#{day[4..5]}-#{day[6..7]} #{time}"
-  end
-  
-  def length=len
-    @length = len
   end
 
   def limits(upper, lower)
     @limits = [upper,lower]
   end
 
-  def max_hr=hr
-    @max_hr = hr
-  end
-
   def summary
     "Date: #{@date}\n" + 
     "Lenght: #{@length}\n" + 
     "Limits: [#{@limits[0]}, #{@limits[1]}]\n" +
-    "Max Heart Rate: #{@max_hr}"
+    "Max Heart Rate: #{@max_hr}\n" + 
+    "Resting Heart Rate: #{@resting_hr}\n" +
+    "VO2 Max: #{@vo2_max}\n" +
+    "Weight: #{@weight}\n" + 
+    "HR Data: \n[#{@hr_data.join(",")}]"
   end
 end
 
-exersice = Exersice.new
+class ExersiceBuilder
+  def self.build(file)
 
-file = File.open("test.hrm", "r").read
-exersice.length = file[/Length=(.+)$/i, 1]
-exersice.limits(file[/Upper1=(.+)$/i, 1], file[/Lower1=(.+)$/i, 1])
-exersice.max_hr = file[/MaxHR=([\d]+)$/i, 1]
-puts "Resting HR: " + file[/RestHR=([\d]+)$/i, 1]
-puts "VO2max: " + file[/VO2max=([\d]+)$/i, 1]
-puts "Weight: " + file[/Weight=([\d]+)$/i, 1]
+    exersice = Exersice.new
+    exersice.date(file[/Date=(.+)$/i, 1], file[/StartTime=(.+)$/i, 1])
+    exersice.length = file[/Length=(.+)$/i, 1]
+    exersice.limits(file[/Upper1=(.+)$/i, 1], file[/Lower1=(.+)$/i, 1])
+    exersice.max_hr = file[/MaxHR=([\d]+)$/i, 1]
+    exersice.resting_hr = file[/RestHR=([\d]+)$/i, 1]
+    exersice.vo2_max = file[/VO2max=([\d]+)$/i, 1]
+    exersice.weight = file[/Weight=([\d]+)$/i, 1]
 
+    #Not that this wont work if height data is present
+    exersice.hr_data = file[file.index(/HRData\]/) + 8..file.size].split("\n")
+    exersice
+  end
+end
 
-exersice.date(file[/Date=(.+)$/i, 1], file[/StartTime=(.+)$/i, 1])
+exersice = ExersiceBuilder.build(File.open("test.hrm", "r").read)
 puts exersice.summary
