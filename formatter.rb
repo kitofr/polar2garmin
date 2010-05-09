0dadf2038be8372dfe10e0bf2f75a9562b9fb27a
kitofr <kitofr@gmail.com>
2010-05-09 16:14:38 +0200

    Removed some output from formatter

--- formatter.rb
+++ formatter.rb
@@ -10,19 +10,5 @@ end
 file = File.new(ARGV[0])
 doc = REXML::Document.new(file)
 formatter = REXML::Formatters::Pretty.new(2)
-#formatter.write(doc, $stdout)
-
-include REXML
-i = 100
-doc.elements.each('//trkseg/trkpt') do |element|
-
-  element.get_elements('./extensions/gpxtpx:TrackPointExtension').each do |ext|
-    hr = Element.new 'gpxtpx:hr'
-    i += 1
-    hr.text = i
-    ext.add_element hr
-  end
-end
-
 formatter.write(doc, $stdout)
 
